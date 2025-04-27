efb415684c9a58396daf897cb3a5ba154c5c5e9d
Bharati <kishorkumar@kishors-Mac-mini.local>
2025-04-28 03:55:43 +0530

    removed torch

--- chat_bot_final/backend/libraries/PathRAG/PathRAG/llm.py
+++ chat_bot_final/backend/libraries/PathRAG/PathRAG/llm.py
@@ -10,7 +10,7 @@ import aioboto3
 import aiohttp
 import numpy as np
 import ollama
-import torch
+# import torch
 import time
 from openai import (
     AsyncOpenAI,
@@ -26,7 +26,7 @@ from tenacity import (
     wait_exponential,
     retry_if_exception_type,
 )
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from .utils import (
     wrap_embedding_func_with_attrs,
@@ -221,18 +221,18 @@ async def bedrock_complete_if_cache(
     return response["output"]["message"]["content"][0]["text"]
 
 
-@lru_cache(maxsize=1)
-def initialize_hf_model(model_name):
-    hf_tokenizer = AutoTokenizer.from_pretrained(
-        model_name, device_map="auto", trust_remote_code=True
-    )
-    hf_model = AutoModelForCausalLM.from_pretrained(
-        model_name, device_map="auto", trust_remote_code=True
-    )
-    if hf_tokenizer.pad_token is None:
-        hf_tokenizer.pad_token = hf_tokenizer.eos_token
+# @lru_cache(maxsize=1)
+# def initialize_hf_model(model_name):
+#     hf_tokenizer = AutoTokenizer.from_pretrained(
+#         model_name, device_map="auto", trust_remote_code=True
+#     )
+#     hf_model = AutoModelForCausalLM.from_pretrained(
+#         model_name, device_map="auto", trust_remote_code=True
+#     )
+#     if hf_tokenizer.pad_token is None:
+#         hf_tokenizer.pad_token = hf_tokenizer.eos_token
 
-    return hf_model, hf_tokenizer
+#     return hf_model, hf_tokenizer
 
 
 @retry(
@@ -240,66 +240,66 @@ def initialize_hf_model(model_name):
     wait=wait_exponential(multiplier=1, min=4, max=10),
     retry=retry_if_exception_type((RateLimitError, APIConnectionError, Timeout)),
 )
-async def hf_model_if_cache(
-    model,
-    prompt,
-    system_prompt=None,
-    history_messages=[],
-    **kwargs,
-) -> str:
-    model_name = model
-    hf_model, hf_tokenizer = initialize_hf_model(model_name)
-    messages = []
-    if system_prompt:
-        messages.append({"role": "system", "content": system_prompt})
-    messages.extend(history_messages)
-    messages.append({"role": "user", "content": prompt})
-    kwargs.pop("hashing_kv", None)
-    input_prompt = ""
-    try:
-        input_prompt = hf_tokenizer.apply_chat_template(
-            messages, tokenize=False, add_generation_prompt=True
-        )
-    except Exception:
-        try:
-            ori_message = copy.deepcopy(messages)
-            if messages[0]["role"] == "system":
-                messages[1]["content"] = (
-                    "<system>"
-                    + messages[0]["content"]
-                    + "</system>\n"
-                    + messages[1]["content"]
-                )
-                messages = messages[1:]
-                input_prompt = hf_tokenizer.apply_chat_template(
-                    messages, tokenize=False, add_generation_prompt=True
-                )
-        except Exception:
-            len_message = len(ori_message)
-            for msgid in range(len_message):
-                input_prompt = (
-                    input_prompt
-                    + "<"
-                    + ori_message[msgid]["role"]
-                    + ">"
-                    + ori_message[msgid]["content"]
-                    + "</"
-                    + ori_message[msgid]["role"]
-                    + ">\n"
-                )
+# async def hf_model_if_cache(
+#     model,
+#     prompt,
+#     system_prompt=None,
+#     history_messages=[],
+#     **kwargs,
+# ) -> str:
+#     model_name = model
+#     hf_model, hf_tokenizer = initialize_hf_model(model_name)
+#     messages = []
+#     if system_prompt:
+#         messages.append({"role": "system", "content": system_prompt})
+#     messages.extend(history_messages)
+#     messages.append({"role": "user", "content": prompt})
+#     kwargs.pop("hashing_kv", None)
+#     input_prompt = ""
+#     try:
+#         input_prompt = hf_tokenizer.apply_chat_template(
+#             messages, tokenize=False, add_generation_prompt=True
+#         )
+#     except Exception:
+#         try:
+#             ori_message = copy.deepcopy(messages)
+#             if messages[0]["role"] == "system":
+#                 messages[1]["content"] = (
+#                     "<system>"
+#                     + messages[0]["content"]
+#                     + "</system>\n"
+#                     + messages[1]["content"]
+#                 )
+#                 messages = messages[1:]
+#                 input_prompt = hf_tokenizer.apply_chat_template(
+#                     messages, tokenize=False, add_generation_prompt=True
+#                 )
+#         except Exception:
+#             len_message = len(ori_message)
+#             for msgid in range(len_message):
+#                 input_prompt = (
+#                     input_prompt
+#                     + "<"
+#                     + ori_message[msgid]["role"]
+#                     + ">"
+#                     + ori_message[msgid]["content"]
+#                     + "</"
+#                     + ori_message[msgid]["role"]
+#                     + ">\n"
+#                 )
 
-    input_ids = hf_tokenizer(
-        input_prompt, return_tensors="pt", padding=True, truncation=True
-    ).to("cuda")
-    inputs = {k: v.to(hf_model.device) for k, v in input_ids.items()}
-    output = hf_model.generate(
-        **input_ids, max_new_tokens=512, num_return_sequences=1, early_stopping=True
-    )
-    response_text = hf_tokenizer.decode(
-        output[0][len(inputs["input_ids"][0]) :], skip_special_tokens=True
-    )
+#     input_ids = hf_tokenizer(
+#         input_prompt, return_tensors="pt", padding=True, truncation=True
+#     ).to("cuda")
+#     inputs = {k: v.to(hf_model.device) for k, v in input_ids.items()}
+#     output = hf_model.generate(
+#         **input_ids, max_new_tokens=512, num_return_sequences=1, early_stopping=True
+#     )
+#     response_text = hf_tokenizer.decode(
+#         output[0][len(inputs["input_ids"][0]) :], skip_special_tokens=True
+#     )
 
-    return response_text
+#     return response_text
 
 
 @retry(
@@ -564,21 +564,21 @@ async def bedrock_complete(
     return result
 
 
-async def hf_model_complete(
-    prompt, system_prompt=None, history_messages=[], keyword_extraction=False, **kwargs
-) -> str:
-    keyword_extraction = kwargs.pop("keyword_extraction", None)
-    model_name = kwargs["hashing_kv"].global_config["llm_model_name"]
-    result = await hf_model_if_cache(
-        model_name,
-        prompt,
-        system_prompt=system_prompt,
-        history_messages=history_messages,
-        **kwargs,
-    )
-    if keyword_extraction:  # TODO: use JSON API
-        return locate_json_string_body_from_string(result)
-    return result
+# async def hf_model_complete(
+#     prompt, system_prompt=None, history_messages=[], keyword_extraction=False, **kwargs
+# ) -> str:
+#     keyword_extraction = kwargs.pop("keyword_extraction", None)
+#     model_name = kwargs["hashing_kv"].global_config["llm_model_name"]
+#     result = await hf_model_if_cache(
+#         model_name,
+#         prompt,
+#         system_prompt=system_prompt,
+#         history_messages=history_messages,
+#         **kwargs,
+#     )
+#     if keyword_extraction:  # TODO: use JSON API
+#         return locate_json_string_body_from_string(result)
+#     return result
 
 
 async def ollama_model_complete(
@@ -980,18 +980,18 @@ async def bedrock_embedding(
         return np.array(embed_texts)
 
 
-async def hf_embedding(texts: list[str], tokenizer, embed_model) -> np.ndarray:
-    device = next(embed_model.parameters()).device
-    input_ids = tokenizer(
-        texts, return_tensors="pt", padding=True, truncation=True
-    ).input_ids.to(device)
-    with torch.no_grad():
-        outputs = embed_model(input_ids)
-        embeddings = outputs.last_hidden_state.mean(dim=1)
-    if embeddings.dtype == torch.bfloat16:
-        return embeddings.detach().to(torch.float32).cpu().numpy()
-    else:
-        return embeddings.detach().cpu().numpy()
+# async def hf_embedding(texts: list[str], tokenizer, embed_model) -> np.ndarray:
+#     device = next(embed_model.parameters()).device
+#     input_ids = tokenizer(
+#         texts, return_tensors="pt", padding=True, truncation=True
+#     ).input_ids.to(device)
+#     with torch.no_grad():
+#         outputs = embed_model(input_ids)
+#         embeddings = outputs.last_hidden_state.mean(dim=1)
+#     if embeddings.dtype == torch.bfloat16:
+#         return embeddings.detach().to(torch.float32).cpu().numpy()
+#     else:
+#         return embeddings.detach().cpu().numpy()
 
 
 async def ollama_embedding(texts: list[str], embed_model, **kwargs) -> np.ndarray:
